001cadf2f98ace2fa94a49259c9bf65b44a021ab
Martijn Schouten <martijn.schouten@icloud.com>
2021-01-18 16:42:04 +0100

    chore: finished lambda function

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    'host': 'xxxx.xxxx.<region>.rds.amazonaws.com',
+    'port': '<port>',
+    'username': '<dbuser>',
+    'password': '<dbpassword>',
+    'database': '<dbname>',
+}
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+const config = require('./config');
+const { Client } = require('pg');
+
+const connectionString = `postgres://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`;
+
+exports.handler = async (event, context) => {
+    const client = new Client({
+        connectionString: connectionString,
+    });
+    await client.connect();
+
+    try {
+        // Get the ccurrent timestamp to store creation date in database
+        const currentISODate = new Date(Date.now()).toISOString();
+
+        // Get the sub (uuid) of the new Cognito user
+        if (!event.request.userAttributes.sub) {
+            // No UUID available, stop execution
+            throw 'No UUID available';
+        }
+        const sub = event.request.userAttributes.sub;
+
+        await client.query(`INSERT INTO cognito_user (sub, created_at) VALUES ('${sub}', '${currentISODate}')`);
+    } catch (error) {
+        console.log(error);
+    }
+
+    client.end();
+    context.done(null, event);
 };
